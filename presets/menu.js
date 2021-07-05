1cd1cdaa6586f9a3a90912ebef59763997052563
Camila <camilasanchezvinik@hotmail.com>
2021-07-05 14:34:14 -0300

    Project started.

---
+++ presets/menu.js
@@ -0,0 +1,38 @@
+const menuButton = document.getElementById('menu-button')
+const menu = document.getElementById('menu')
+const main = document.getElementsByTagName('main')[0]
+const footer = document.getElementsByTagName('footer')[0]
+
+let open = false // this will determine if the menu is open or not
+
+function closeSettings() {
+    menu.style.width = '0';
+    menuButton.style.color = '#00afb9'
+    open = false;
+}
+
+// open menu function
+function openButton() {
+    if (!open) {
+        menu.style.width = '425px';
+        menuButton.style.color = '#f07167'
+        open = true;
+
+        // add listener for closing menu
+        main.addEventListener('click', closeMenu);
+        footer.addEventListener('click', closeMenu);
+    } else {
+        closeSettings()
+    }
+}
+
+// close menu function
+function closeMenu() {
+    closeSettings()
+
+    // remove listener for closing menu
+    main.removeEventListener('click', closeMenu);
+    footer.removeEventListener('click', closeMenu);
+}
+
+menuButton.addEventListener('click', openButton)
---
+++ screens/match/script.js
@@ -0,0 +1,39 @@
+// Team 1 consts
+const redThrees1 = document.getElementsByClassName('red-threes-1')
+const blacks1 = document.getElementById('black1')
+const reds1 = document.getElementById('red1')
+const others1 = document.getElementById('other1')
+const calculator1 = document.getElementById('points-calculator-1')
+const points1 = document.getElementById('points1')
+
+// Team 2 consts
+const redThrees2 = document.getElementsByClassName('red-threes-2')
+const blacks2 = document.getElementById('black2')
+const reds2 = document.getElementById('red2')
+const others2 = document.getElementById('other2')
+const calculator2 = document.getElementById('points-calculator-2')
+const points2 = document.getElementById('points2')
+
+function setPoints(threes, blacks, reds, others, result) {
+    let points = 0;
+
+    for (let i = 0; i < threes.length; i++) {
+        if (threes[i].checked) {
+            points += 100
+        }
+    }
+    if (points === 400) points *= 2
+    if (blacks.value) points += parseInt(blacks.value) * 100
+    if (reds.value) points += parseInt(reds.value) * 300
+    if (others.value) points += parseInt(others.value)
+
+    result.innerHTML = points.toString()
+}
+
+calculator1.addEventListener('click', function () {
+    setPoints(redThrees1, blacks1, reds1, others1, points1)
+})
+
+calculator2.addEventListener('click', function () {
+    setPoints(redThrees2, blacks2, reds2, others2, points2)
+})
